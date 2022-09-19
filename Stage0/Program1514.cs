63b454ddf945cba4cd4218c3bbb3f3a0fb5c16e4
Ayala-k <kluftayala@gmail.com>
2022-09-19 21:31:42 +0300

    Tamar& Ayala first step

---
+++ Stage0/Program1514.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Wellcome1514();
+            Wellcome5726();
+            Console.ReadKey();
+        }
+
+        static partial void Wellcome5726();
+        private static void Wellcome1514()
+        {
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+    }
+}
