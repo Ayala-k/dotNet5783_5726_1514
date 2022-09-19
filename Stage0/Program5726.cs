34156d8d72989a6b77d5741f8f19a761d7ae754f
tamarcohen32 <tamarcohen32@gmail.com>
2022-09-19 22:17:09 +0300

    targil0

---
+++ Stage0/Program5726.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace targil0
+ //Stage0
+{
+ partial class Program
+ {
+  static partial void Wellcome5726()
+  {
+   Console.WriteLine("Welllcome");
+  } 
+ }
+}
