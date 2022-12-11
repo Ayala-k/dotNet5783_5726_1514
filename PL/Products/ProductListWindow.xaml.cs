d53e20d80d7bd3e96d6cbce10fa9f13fe24d0151
Ayala-k <kluftayala@gmail.com>
2022-12-11 02:15:39 +0200

    motsash2

--- PL/Products/ProductListWindow.xaml.cs
+++ PL/Products/ProductListWindow.xaml.cs
@@ -2,18 +2,8 @@
 using BL.BO;
 using PL.Products;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace PL;
 
@@ -22,35 +12,53 @@ namespace PL;
 /// </summary>
 public partial class ProductListWindow : Window
 {
- private IBl bl = new BlImplementation.Bl();
- public ProductListWindow()
- {
-  InitializeComponent();
-  ProductListview.ItemsSource = bl.Product.GetProducts();
-  CategoriesSelector.ItemsSource = Enum.GetValues(typeof(Categories));
- }
+    private IBl bl = new BlImplementation.Bl();
+    public ProductListWindow()
+    {
+        InitializeComponent();
+        ProductListview.ItemsSource = bl.Product.GetProducts();
+        CategoriesSelector.ItemsSource = Enum.GetValues(typeof(Categories));
+    }
 
- private void CategoriesSelector_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
- {
-  if (CategoriesSelector.Text != " ")
-   ProductListview.ItemsSource = bl.Product.GetProducts
- (p => p.Category == (DO.Categories)CategoriesSelector.SelectedItem);
- }
- private void ProductListview_SelectionChanged(object sender, SelectionChangedEventArgs e)
- {
-  int id = 0;
-  if (ProductListview.SelectedItem is BL.BO.ProductForList)
-   id = ((BL.BO.ProductForList)ProductListview.SelectedItem).ID;
-  new ProductWindow("update", id).ShowDialog();
- }
- private void Button_Click(object sender, RoutedEventArgs e)
- {
-  new ProductWindow("add").ShowDialog();
-  //ProductListview.ItemsSource = bl.Product.GetProducts();
- }
- private void Button_Click_1(object sender, RoutedEventArgs e)
- {
-  ProductListview.ItemsSource = bl.Product.GetProducts();
-  CategoriesSelector.Text = " ";
- }
+    /// <summary>
+    /// category selector
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void CategoriesSelector_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+    {
+        if (CategoriesSelector.Text != " ")
+            ProductListview.ItemsSource = bl.Product.GetProducts
+          (p => p.Category == (DO.Categories)CategoriesSelector.SelectedItem);
+    }
+    
+    /// <summary>
+    /// when a product is clicked- upadte it
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void ProductListview_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        int id = 0;
+        if (ProductListview.SelectedItem is BL.BO.ProductForList)
+            id = ((BL.BO.ProductForList)ProductListview.SelectedItem).ID;
+        new ProductWindow("update", id).ShowDialog();
+    }
+    
+    /// <summary>
+    /// when "add product" is clicked- open add window
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        new ProductWindow("add").ShowDialog();
+    }
+
+    //show all products
+    private void Button_Click_1(object sender, RoutedEventArgs e)
+    {
+        ProductListview.ItemsSource = bl.Product.GetProducts();
+        CategoriesSelector.Text = " ";
+    }
 }
--- PL/Products/ProductWindow.xaml.cs
+++ PL/Products/ProductWindow.xaml.cs
@@ -1,125 +1,150 @@
 ﻿using BL.BlApi;
 using BL.BO;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-using System.Text.RegularExpressions;
-
 
 namespace PL.Products
 {
- /// <summary>
- /// Interaction logic for ProductWindow.xaml
- /// </summary>
- public partial class ProductWindow : Window
- {
-  private IBl bl = new BlImplementation.Bl();
-
-  public ProductWindow()
-  {
-   InitializeComponent();
-   CategoriesSelector.ItemsSource = Enum.GetValues(typeof(Categories));
-  }
-  public ProductWindow(string str)
-  {
-   InitializeComponent();
-   if (str == "add")
-    buttonAddUpdate.Content = "add";
-   CategoriesSelector.ItemsSource = Enum.GetValues(typeof(Categories));
-
-  }
-  public ProductWindow(string str, int productId)
-  {
-   Product product = bl.Product.GetProductDetailsManager(productId);
-   InitializeComponent();
-   if (str == "update")
-    buttonAddUpdate.Content = "update";
-   CategoriesSelector.ItemsSource = Enum.GetValues(typeof(Categories));
-   id.Text = Convert.ToString(productId);
-   id.IsReadOnly = true;
-   name.Text = product.Name;
-   price.Text = Convert.ToString(product.Price);
-   //CategoriesSelector.Text = product.Category;
-   CategoriesSelector.Text = product.Category.ToString();
-   inStock.Text = Convert.ToString(product.InStock);
-  }
-
-  private void inStock_TextChanged(object sender, TextChangedEventArgs e)
-  {
-
-  }
-
-  private void buttonAddUpdate_Click(object sender, RoutedEventArgs e)
-  {
-   Product product = new Product()
-   {
-    ID = Convert.ToInt32(id.Text),//??
-    Name = name.Text,
-    Category = (BL.BO.Categories)CategoriesSelector.SelectedItem,
-    Price = Convert.ToDouble(price.Text),
-    InStock = Convert.ToInt32(inStock.Text)
-   };
-   if (buttonAddUpdate.Content == "add")
-    try
+    /// <summary>
+    /// Interaction logic for ProductWindow.xaml
+    /// </summary>
+    public partial class ProductWindow : Window
     {
-     bl.Product.AddProduct(product);
-    }
-    catch (InvalidDetailsException exp)
-    {
-     errorMessage.Content = exp.Message.ToString();
-    }
-    catch (EntityAlreadyExistsLogicException exp)
-    {
-     errorMessage.Content = exp.Message.ToString();
-    }
-   else
-    try
-    {
-     bl.Product.UpdateProduct(product);
-    }
-    catch (InvalidDetailsException exp)
-    {
-     errorMessage.Content = exp.Message.ToString();
-    }
-    catch (EntityNotFoundLogicException exp)
-    {
-     errorMessage.Content = exp.Message.ToString();
+        private IBl bl = new BlImplementation.Bl();
 
-    }
-   if (errorMessage.Content == "")
-   {
-    this.Close();
-    foreach (Window w in Application.Current.Windows)
-    {
-     if (w is ProductListWindow)
-     {
-      w.Close();
-     }
-    }
-    new ProductListWindow().Show();
-   }
+        public ProductWindow()
+        {
+            InitializeComponent();
+            CategoriesSelector.ItemsSource = Enum.GetValues(typeof(Categories));
+        }
 
-  }
-  private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-  {
+        /// <summary>
+        /// product window ctor for add
+        /// </summary>
+        /// <param name="str">str=add to make sure the action to be done is adding</param>
+        public ProductWindow(string str)
+        {
+            InitializeComponent();
+            if (str == "add")
+                buttonAddUpdate.Content = "add";
+            CategoriesSelector.ItemsSource = Enum.GetValues(typeof(Categories));
 
-  }
-  private void PreviewTextInput(object sender, TextCompositionEventArgs e)
-  {
-   Regex regex = new Regex("[^0-9]+");
-   e.Handled = regex.IsMatch(e.Text);
-  }
- }
+        }
+
+        /// <summary>
+        /// product window ctor for update
+        /// </summary>
+        /// <param name="str">str=update to make sure the action to be done is updating</param>
+        /// <param name="productId">product to be updated</param>
+        public ProductWindow(string str, int productId)
+        {
+            Product product = new Product();
+            try
+            {
+                product = bl.Product.GetProductDetailsManager(productId);
+            }
+            catch (EntityNotFoundLogicException exp)
+            {
+                errorMessage.Content = exp.Message;
+            }
+            catch (InvalidDetailsException exp)
+            {
+                errorMessage.Content = exp.Message;
+            }
+
+            InitializeComponent();
+            if (str == "update")
+                buttonAddUpdate.Content = "update";
+
+            CategoriesSelector.ItemsSource = Enum.GetValues(typeof(Categories));
+            id.Text = Convert.ToString(productId);
+            id.IsReadOnly = true;
+            name.Text = product.Name;
+            price.Text = Convert.ToString(product.Price);
+            CategoriesSelector.Text = product.Category.ToString();
+            inStock.Text = Convert.ToString(product.InStock);
+        }
+
+        private void inStock_TextChanged(object sender, TextChangedEventArgs e) { }
+
+        /// <summary>
+        /// add or update
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonAddUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            Product product = new Product()
+            {
+                ID = Convert.ToInt32(id.Text),
+                Name = name.Text,
+                Category = (BL.BO.Categories)CategoriesSelector.SelectedItem,
+                Price = Convert.ToDouble(price.Text),
+                InStock = Convert.ToInt32(inStock.Text)
+            };
+
+            errorMessage.Content = "";
+
+            //add
+            if (buttonAddUpdate.Content == "add")
+                try
+                {
+                    bl.Product.AddProduct(product);
+                }
+                catch (InvalidDetailsException exp)
+                {
+                    errorMessage.Content = exp.Message.ToString();
+                }
+                catch (EntityAlreadyExistsLogicException exp)
+                {
+                    errorMessage.Content = exp.Message.ToString();
+                }
+            //update
+            else
+                try
+                {
+                    bl.Product.UpdateProduct(product);
+                }
+                catch (InvalidDetailsException exp)
+                {
+                    errorMessage.Content = exp.Message.ToString();
+                }
+                catch (EntityNotFoundLogicException exp)
+                {
+                    errorMessage.Content = exp.Message.ToString();
+
+                }
+
+            //if the action has been done
+            if (errorMessage.Content == "")
+            {
+                this.Close();
+                foreach (Window w in Application.Current.Windows)
+                {
+                    if (w is ProductListWindow)
+                    {
+                        w.Close();
+                    }
+                }
+                new ProductListWindow().Show();
+            }
+
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e){}
+
+        /// <summary>
+        /// prevent letters in numeric input fields
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+    }
 }
