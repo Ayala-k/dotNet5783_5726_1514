0b0b035e93a76c9a4a5c27e7f86dde67968cc968
tamarcohen32 <tamarcohen32@gmail.com>
2023-01-03 19:22:12 +0200

    tuesday1

--- PL/manager/Products/AddUpdateProductWindow.xaml.cs
+++ PL/manager/Products/AddUpdateProductWindow.xaml.cs
@@ -9,146 +9,139 @@ using System.Windows.Input;
 
 namespace PL.Products
 {
-    /// <summary>
-    /// Interaction logic for ProductWindow.xaml
-    /// </summary>
-    public partial class ProductWindow : Window
+ /// <summary>
+ /// Interaction logic for ProductWindow.xaml
+ /// </summary>
+
+
+ public partial class ProductWindow : Window
+ {
+  BL.BlApi.IBl? bl = BlApi.Factory.Get();
+  public static Product product { get; set; }
+  public static string pageName { get; set; }
+  public static System.Array categories { get; set; }= Enum.GetValues(typeof(Categories));
+
+  public ProductWindow()
+  {
+   InitializeComponent();
+  }
+
+  /// <summary>
+  /// product window ctor for add
+  /// </summary>
+  /// <param name="str">str=add to make sure the action to be done is adding</param>
+
+  public ProductWindow(string str)
+  {
+   if (str == "add")
+   {
+    pageName = "add";
+   }
+   InitializeComponent();
+  }
+
+  /// <summary>
+  /// product window ctor for update
+  /// </summary>
+  /// <param name="str">str=update to make sure the action to be done is updating</param>
+  /// <param name="productId">product to be updated</param>
+
+  public ProductWindow(string str, int productId)
+  {
+   product = bl.Product.GetProductDetailsManager(productId);
+   if (str == "update")
+   {
+    pageName = "update";
+   }
+   InitializeComponent();
+  }
+
+  private void inStock_TextChanged(object sender, TextChangedEventArgs e) { }
+
+  /// <summary>
+  /// add or update
+  /// </summary>
+  /// <param name="sender"></param>
+  /// <param name="e"></param>
+
+  private void buttonAddUpdate_Click(object sender, RoutedEventArgs e)
+  {
+   errorMessage.Content = "";
+   if (id.Text == "" || name.Text == "" || CategoriesSelector.SelectedItem == null
+    || price.Text == "" || inStock.Text == "")
+   {
+    errorMessage.Content = "please fill in all fields";
+   }
+   else
+   {
+    //Product product = new Product()
+    //{
+    // ID = Convert.ToInt32(id.Text),
+    // Name = name.Text,
+    // Category = (BL.BO.Categories)CategoriesSelector.SelectedItem,
+    // Price = Convert.ToDouble(price.Text),
+    // InStock = Convert.ToInt32(inStock.Text)
+    //};
+
+    //add
+    if (pageName == "add")
+     try
+     {
+      bl.Product.AddProduct(product);
+     }
+     catch (InvalidDetailsException exp)
+     {
+      errorMessage.Content = exp.Message.ToString();
+     }
+     catch (EntityAlreadyExistsLogicException exp)
+     {
+      errorMessage.Content = exp.Message.ToString();
+     }
+    //update
+    else
+     try
+     {
+      bl.Product.UpdateProduct(product);
+     }
+     catch (InvalidDetailsException exp)
+     {
+      errorMessage.Content = exp.Message.ToString();
+     }
+     catch (EntityNotFoundLogicException exp)
+     {
+      errorMessage.Content = exp.Message.ToString();
+
+     }
+
+   }
+
+   //if the action has been done
+   if (errorMessage.Content == "")
+   {
+    this.Close();
+    foreach (Window w in Application.Current.Windows)
     {
-        BL.BlApi.IBl? bl = BlApi.Factory.Get();
-        public ProductWindow()
-        {
-            InitializeComponent();
-            CategoriesSelector.ItemsSource = Enum.GetValues(typeof(Categories));
-        }
-
-        /// <summary>
-        /// product window ctor for add
-        /// </summary>
-        /// <param name="str">str=add to make sure the action to be done is adding</param>
-
-        public ProductWindow(string str)
-        {
-            InitializeComponent();
-            if (str == "add")
-            {
-                buttonAddUpdate.Content = "add";
-                pageName.Content = "add product";
-            }
-            CategoriesSelector.ItemsSource = Enum.GetValues(typeof(Categories));
-
-        }
-
-        /// <summary>
-        /// product window ctor for update
-        /// </summary>
-        /// <param name="str">str=update to make sure the action to be done is updating</param>
-        /// <param name="productId">product to be updated</param>
-
-        public ProductWindow(string str, int productId)
-        {
-            Product product = bl.Product.GetProductDetailsManager(productId);
-            InitializeComponent();
-            if (str == "update")
-            {
-                buttonAddUpdate.Content = "update";
-                pageName.Content = "update product";
-            }
-            CategoriesSelector.ItemsSource = Enum.GetValues(typeof(Categories));
-            id.Text = Convert.ToString(productId);
-            id.IsReadOnly = true;
-            name.Text = product.Name;
-            price.Text = Convert.ToString(product.Price);
-            //CategoriesSelector.Text = product.Category;
-            CategoriesSelector.Text = product.Category.ToString();
-            inStock.Text = Convert.ToString(product.InStock);
-        }
-
-        private void inStock_TextChanged(object sender, TextChangedEventArgs e) { }
-
-        /// <summary>
-        /// add or update
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-
-        private void buttonAddUpdate_Click(object sender, RoutedEventArgs e)
-        {
-            errorMessage.Content = "";
-            if (id.Text == "" || name.Text == "" || CategoriesSelector.SelectedItem == null
-             || price.Text == "" || inStock.Text == "")
-            {
-                errorMessage.Content = "please fill in all fields";
-            }
-            else
-            {
-                Product product = new Product()
-                {
-                    ID = Convert.ToInt32(id.Text),
-                    Name = name.Text,
-                    Category = (BL.BO.Categories)CategoriesSelector.SelectedItem,
-                    Price = Convert.ToDouble(price.Text),
-                    InStock = Convert.ToInt32(inStock.Text)
-                };
-
-                //add
-                if (buttonAddUpdate.Content == "add")
-                    try
-                    {
-                        bl.Product.AddProduct(product);
-                    }
-                    catch (InvalidDetailsException exp)
-                    {
-                        errorMessage.Content = exp.Message.ToString();
-                    }
-                    catch (EntityAlreadyExistsLogicException exp)
-                    {
-                        errorMessage.Content = exp.Message.ToString();
-                    }
-                //update
-                else
-                    try
-                    {
-                        bl.Product.UpdateProduct(product);
-                    }
-                    catch (InvalidDetailsException exp)
-                    {
-                        errorMessage.Content = exp.Message.ToString();
-                    }
-                    catch (EntityNotFoundLogicException exp)
-                    {
-                        errorMessage.Content = exp.Message.ToString();
-
-                    }
-
-            }
-
-            //if the action has been done
-            if (errorMessage.Content == "")
-            {
-                this.Close();
-                foreach (Window w in Application.Current.Windows)
-                {
-                    if (w is ProductListWindow)
-                    {
-                        w.Close();
-                    }
-                }
-                new ProductListWindow().Show();
-            }
-
-        }
-
-        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e) { }
-
-        /// <summary>
-        /// prevent letters in numeric input fields
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void PreviewTextInput(object sender, TextCompositionEventArgs e)
-        {
-            Regex regex = new Regex("[^0-9]+");
-            e.Handled = regex.IsMatch(e.Text);
-        }
+     if (w is ProductListWindow)
+     {
+      w.Close();
+     }
     }
+    new ProductListWindow().Show();
+   }
+
+  }
+
+  private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e) { }
+
+  /// <summary>
+  /// prevent letters in numeric input fields
+  /// </summary>
+  /// <param name="sender"></param>
+  /// <param name="e"></param>
+  private void PreviewTextInput(object sender, TextCompositionEventArgs e)
+  {
+   Regex regex = new Regex("[^0-9]+");
+   e.Handled = regex.IsMatch(e.Text);
+  }
+ }
 }
--- PL/manager/Products/ProductListWindow.xaml.cs
+++ PL/manager/Products/ProductListWindow.xaml.cs
@@ -14,69 +14,88 @@ namespace PL.Products;
 /// </summary>
 public partial class ProductListWindow : Window
 {
-    
-    BL.BlApi.IBl? bl = BlApi.Factory.Get();
 
-    public static ObservableCollection<ProductForList?> productsForListList { get; set; } = new ObservableCollection<ProductForList?>();
+ BL.BlApi.IBl? bl = BlApi.Factory.Get();
 
-
-    public ObservableCollection<T> Convert<T>(IEnumerable<T> original)
-    {
-        return new ObservableCollection<T>(original);
-    }
-
-    public ProductListWindow()
-    {
-        InitializeComponent();
-        productsForListList = Convert(bl.Product.GetProducts());
-
-        ProductListview.ItemsSource = productsForListList;
-        CategoriesSelector.ItemsSource = Enum.GetValues(typeof(Categories));
-    }
-
-    /// <summary>
-    /// category selector
-    /// </summary>
-    /// <param name="sender"></param>
-    /// <param name="e"></param>
+ //public static IEnumerable<ProductForList?> productsForListList { get; set; } = new List<ProductForList?>();
+ public static readonly DependencyProperty productsForListListProperty =
+    DependencyProperty.Register(name:"productsForListList", propertyType:typeof(IEnumerable<ProductForList?>),ownerType:typeof(ProductListWindow));
+ public IEnumerable<ProductForList?> productsForListList
+ {
+  get { return (IEnumerable<ProductForList?>)GetValue(productsForListListProperty); }
+  set { SetValue(productsForListListProperty, value); }
+ }
 
 
-    private void CategoriesSelector_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
-    {
-        if (CategoriesSelector.Text != " ")
-            ProductListview.ItemsSource = bl.Product.GetProducts
-          (p => p.Category == (DO.Categories)CategoriesSelector.SelectedItem);
-    }
+
+ //public int MyProperty//דוגמא
+ //{
+ // get { return (int)GetValue(MyPropertyProperty); }
+ // set { SetValue(MyPropertyProperty, value); }
+ //}
+
+ // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
 
 
-    /// <summary>
-    /// when a product is clicked- upadte it
-    /// </summary>
-    /// <param name="sender"></param>
-    /// <param name="e"></param>
+ public static System.Array categories { get; set; } = Enum.GetValues(typeof(Categories));
 
 
-    private void ProductListview_SelectionChanged(object sender, SelectionChangedEventArgs e)
-    {
-        int id = 0;
-        if (ProductListview.SelectedItem is BL.BO.ProductForList)
-            id = ((BL.BO.ProductForList)ProductListview.SelectedItem).ID;
-        new ProductWindow("update", id).ShowDialog();
-    }
+ //public ObservableCollection<T> Convert<T>(IEnumerable<T> original)
+ //{
+ // return new ObservableCollection<T>(original);
+ //}
 
-    /// <summary>
-    /// when "add product" is clicked- open add window
-    /// </summary>
-    /// <param name="sender"></param>
-    /// <param name="e"></param>
+ public ProductListWindow()
+ {
+  productsForListList =(bl.Product.GetProducts());
+  SetValue(productsForListListProperty, new List<ProductForList?>());
+  SetValue(productsForListListProperty, bl.Product.GetProducts());
+  InitializeComponent();
+ }
 
-    private void Button_Click(object sender, RoutedEventArgs e)
-    {
-        new ProductWindow("add").ShowDialog();
-    }
-    private void Button_Click_1(object sender, RoutedEventArgs e)
-    {
-        ProductListview.ItemsSource = productsForListList;
-        CategoriesSelector.Text = " ";
-    }
+ /// <summary>
+ /// category selector
+ /// </summary>
+ /// <param name="sender"></param>
+ /// <param name="e"></param>
+
+
+ private void CategoriesSelector_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+ {
+  if (CategoriesSelector.Text != " ")
+   productsForListList = bl.Product.GetProducts
+ (p => p.Category == (DO.Categories)CategoriesSelector.SelectedItem);
+ }
+
+
+ /// <summary>
+ /// when a product is clicked- upadte it
+ /// </summary>
+ /// <param name="sender"></param>
+ /// <param name="e"></param>
+
+
+ private void ProductListview_SelectionChanged(object sender, SelectionChangedEventArgs e)
+ {
+  int id = 0;//bindingלשנות ל
+  if (ProductListview.SelectedItem is BL.BO.ProductForList)
+   id = ((BL.BO.ProductForList)ProductListview.SelectedItem).ID;
+  new ProductWindow("update", id).ShowDialog();
+ }
+
+ /// <summary>
+ /// when "add product" is clicked- open add window
+ /// </summary>
+ /// <param name="sender"></param>
+ /// <param name="e"></param>
+
+ private void Button_Click(object sender, RoutedEventArgs e)
+ {
+  new ProductWindow("add").ShowDialog();
+ }
+ private void Button_Click_1(object sender, RoutedEventArgs e)
+ {
+  ProductListview.ItemsSource = productsForListList;
+  CategoriesSelector.Text = " ";
+ }
 }
